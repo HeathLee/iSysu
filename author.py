feeb1a09158df3cb784c88f884d88677fae4e9cc
HeathLee <753940818@qq.com>
2015-11-24 16:59:11 +0800

    完成自定义菜单

--- author.py
+++ author.py
@@ -1,16 +1,98 @@
 # -*- coding: utf-8 -*-
 
-import time
-import pycurl
-import util
-import tornado.web
 import hashlib
-import tornado.ioloop
 import xml.etree.ElementTree as ET
-from io import BytesIO
+
+import tornado.ioloop
+import tornado.web
+
 import replyer
+import util
 
 rules = []
+
+menuData = """
+{
+    "button":[
+        {
+            "name":"i学习",
+            "sub_button":[
+                {
+                    "name":"校内常用网站",
+                    "type":"view",
+                    "url":"http://mp.weixin.qq.com/s?__biz=MzA3MDc5NjAyOA==&mid=205420177&idx=1&sn=127a582cd5adef81f22fdb3560e61672&scene=18#wechat_redirect"
+                },
+                {
+                    "name":"中大校报",
+                    "type":"view",
+                    "url":"http://xiaobao.sysu.edu.cn/"
+                },
+                {
+                    "name":"图书馆",
+                    "type":"view",
+                    "url":"http://library.sysu.edu.cn/"
+                },
+                {
+                    "name":"选课",
+                    "type":"view",
+                    "url":"http://uems.sysu.edu.cn/elect/"
+                }
+            ]
+        },
+        {
+            "name":"i生活",
+            "sub_button":[
+                {
+                   "type":"media_id",
+                   "name":"校车时刻表",
+                   "media_id":"u8uPp_Xv-4FyRVPyTYou7ixpEKbWZC7w-a9bSUDZd19L3L_gnbKRQlwCB3pGIr7V"
+                },
+                {
+                   "type":"view",
+                   "name":"校内公号推荐",
+                   "url":"http://mp.weixin.qq.com/s?__biz=MzA3MDc5NjAyOA==&mid=205422765&idx=1&sn=c86343adb5e1230545137736594c0720&scene=18#wechat_redirect"
+                },
+                {
+                   "type":"view",
+                   "name":"校区地图",
+                   "url":"http://www.sysu.edu.cn/2012/cn/zjzd/zjzd02/index.htm"
+                },
+                {
+                   "type":"view",
+                   "name":"常用电话",
+                   "url":"http://home.sysu.edu.cn/tele/otele.asp"
+                },
+                {
+                   "type":"media_id",
+                   "name":"中大校历",
+                   "media_id":"GI1Tl2pOccBGncHVrwFVwBWzFKhqzmW3J7IIOlYzKADitHqWXBz84VyHwSk3sGl4"
+                }
+            ]
+        },
+         {
+            "name":"i互动",
+            "sub_button":[
+                {
+                   "type":"view",
+                   "name":"欢迎来稿",
+                   "url":"http://mp.weixin.qq.com/s?__biz=MzA3MDc5NjAyOA==&mid=208325182&idx=2&sn=35492466165661ce4323dcddb4b8c9d7&scene=18#wechat_redirect"
+                },
+                {
+                   "type":"text",
+                   "name":"联系我们",
+                   "value":"中大官微iSYSU期待您的踊跃投稿，为您的精彩提供展现平台。我们也欢迎您真诚的反馈与建议，我们立志于更好。来稿请投：zhongdaguanwei@163.com，您对iSYSU的意见和建议可直接回复至微信后台。感谢您对iSYSU的支持！mo-示爱"
+                },
+                {
+                   "type":"view",
+                   "name":"互动游戏",
+                   "url":""
+                },
+            ]
+        }
+    ]
+}
+"""
+
 
 # 微信开发者验证
 class AuthenticationHandler(tornado.web.RequestHandler):
@@ -51,9 +133,10 @@ class AuthenticationHandler(tornado.web.RequestHandler):
                 break
         self.write(out)
 
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello")
+        pass
 
 
 def make_app():
@@ -65,7 +148,7 @@ def make_app():
 if __name__ == "__main__":
     helper = util.Helper('wxb9db0419592dbe7f',
                          '3a54d5663c95800880c927b124a31ead')
-    # helper.get_auto_reply()
+    # helper.getAutoReply()
     rules = replyer.create_rulers('rule.json')
     app = make_app()
     app.listen(80)
